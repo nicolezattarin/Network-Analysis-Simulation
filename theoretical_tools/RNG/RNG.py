50e4f8239ae73d0a1e9bec6ae180fd0bac2d8b1f
nicolezattarin <72666452+nicolezattarin@users.noreply.github.com>
2022-03-22 14:06:18 +0100

    cdf binomial

--- theoretical_tools/RNG/RNG.py
+++ theoretical_tools/RNG/RNG.py
@@ -32,13 +32,12 @@ class random ():
             return rand
 
         if size == None or size == 1:
-            rand = 0
-            size = 1
+            rand = inf + (sup-inf)*uniform(inf,sup,a,b,m)/m 
         else :
             size = int(size)
             rand = np.zeros(size)
-        for i in range(size):
-            rand[i] = inf + (sup-inf)*uniform(0,1,a,b,m)/m
+            for i in range(size):
+                rand[i] = inf + (sup-inf)*uniform(0,1,a,b,m)/m
         return rand
 
 
@@ -77,6 +76,18 @@ class random ():
                 trial = np.random.geometric(p)
                 n_trials += trial + 1 # all the failures + success
             return rand-1
+
+        def binomial_cdf (n,p):
+            u = self.uniform(0,1)
+            c = p/(1-p) 
+            pr = (1-p)**n
+            F = pr
+            rand = 0
+            while u>=F:
+                rand += 1
+                pr += c*(n-rand)/(rand+1)*pr
+                F += pr
+            return rand
         
         if size == None or size == 1:
             rand = 0
@@ -93,6 +104,10 @@ class random ():
             if size != 1:
                 rand = [binomial_geometric(n,p) for i in range(size)]
             else: rand = binomial_geometric(n,p)
+        elif m == 'cdf':
+            if size != 1:
+                rand = [binomial_cdf(n,p) for i in range(size)]
+            else: rand = binomial_cdf(n,p)
 
         return rand
                 
--- theoretical_tools/RNG/test.py
+++ theoretical_tools/RNG/test.py
@@ -4,5 +4,6 @@ import numpy as np
 rand = random(seed=0)
 
 print ("uniform", rand.uniform(0,1,size= 10))
-print ("binomial uniform", rand.binomial (n=100, p=0.2, size=10, method='uniform'))
-print ("binomial geometric", rand.binomial (n=100, p=0.2, size=10, method='geometric'))
+print ("binomial cdf", rand.binomial (n=1000, p=0.2, size=10, method='cdf'))
+print ("binomial uniform", rand.binomial (n=1000, p=0.2, size=10, method='uniform'))
+print ("binomial geometric", rand.binomial (n=1000, p=0.2, size=10, method='geometric'))
